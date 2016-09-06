da470ed31ae0694eef3facb4462a005cb64398d6
AlexToneka <salexandercrist@gmail.com>
2016-09-06 00:11:23 -0500

    First upload

---
+++ internet-research/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Alex Toneka's Online Research</title>
+    <?php include 'styles.php';?>
+</head>  
+<body>
+    
+<div class="container">    
+
+<header>
+    <div style="text-align:center; font-size:150%; color:#004d4d;">Alex Toneka's</div>
+    <div style="text-align:center; font-size:200%; color:#004d4d;">Research URLs</div>
+</header>
+
+<nav>    
+    <ul>
+        <li><a href="index.php" class="active">Frequently Used</a></li>
+        <li><a href="freelance.php">About Freelance</a></li>
+        <li><a href="research.php">Do Research</a></li>
+    </ul>
+</nav> 
+       
+<section id="add_link_form">    
+</section>
+
+<section id="results_table">
+Frequently used links for quick reference. Also, this will serve as a homepage for me. A place I can call home.
+<br>
+<img src="images/161091_mieshka_internet-research - Copy.jpg" style="width:70%;">
+</section>
+    
+</div>   
+
+</body>
+</html>
