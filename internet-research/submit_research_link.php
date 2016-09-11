1e057e48014b3f239051d463023ca5e336d19af4
AlexToneka <salexandercrist@gmail.com>
2016-09-11 12:02:16 -0500

    Add files via upload

--- internet-research/submit_research_link.php
+++ internet-research/submit_research_link.php
@@ -4,17 +4,34 @@ $posturl = mysqli_real_escape_string($connection, strip_tags(trim($_POST['url'])
 $postnotes = mysqli_real_escape_string($connection, strip_tags(trim($_POST['notes'])));
 
 if (isset($_POST['rtags'])) {
-    $posttags = mysqli_real_escape_string($connection, strip_tags(trim(implode(', ',$_POST['rtags']))));
+    $posttags = $_POST['rtags'];
+    $length = count($posttags);
 } else {
-    $posttags = "";
+    $posttags = "19"; //None defined
 }
 
-$sql_insert = "INSERT INTO do_research (name, url, tags, notes)
-    VALUES ('$postname', '$posturl', '$posttags', '$postnotes')";
+$sql_insert1 = 
+    "INSERT INTO rbookmarks (rbm_title, rbm_url, rbm_notes)
+    VALUES ('$postname', '$posturl', '$postnotes')";
 
-if ($connection->query($sql_insert) === TRUE) {
-    echo "<font color=\"#00b300\">Added new link: </font>" . $postname;
-} else {
-    echo "Something went wrong. Link not added.<br>" . $connection->error;
+//$sql_insert2 = 
+    //"INSERT INTO rbm_rtag_map (rbm_map_id, rtag_map_id)
+   // VALUES ('$postname', '$posturl')
+   // WHERE rmap_id = '$rtag'";
+
+if ($connection->query($sql_insert1) === TRUE) {
+        echo "<font color=\"#00b300\">Added new link: </font>" . $postname;
+    } else {
+        echo "Something went wrong. Link not added.<br>" . $connection->error;
 }
+
+$biggest_rbm_id_result = $connection->query(
+    "SELECT rbm_id
+    FROM rbookmarks
+    ORDER BY rbm_id DESC
+    LIMIT 1");
+
+$row = $biggest_rbm_id_result->fetch_assoc();
+echo " (Bookmark #" . $row['rbm_id'] . ")";
+  
 ?>
